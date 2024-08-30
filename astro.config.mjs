d5772e4bfa637c01cccb0a041dcc146612569a75
Sean C Davis <scdavis41@gmail.com>
2024-08-30 06:54:15 -0400

    Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import netlify from '@astrojs/netlify'
+import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   adapter: netlify(),
+  integrations: [tailwind()],
 })
