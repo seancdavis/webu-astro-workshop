f503851ea900dd69252c82bf51745d7331e6a386
Sean C Davis <scdavis41@gmail.com>
2024-09-02 08:57:31 -0400

    Install supabase locally

---
+++ supabase/migrations/20240902125504_create-reviews.sql
@@ -0,0 +1,9 @@
+CREATE TABLE reviews (
+  id SERIAL PRIMARY KEY,
+  product_id VARCHAR NOT NULL,
+  rating INTEGER CHECK (
+    rating BETWEEN 1 AND 5
+  ) NOT NULL,
+  comment TEXT,
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
