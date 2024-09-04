145023913eb68d084f8731c27fa40c5d0e80847e
Sean C Davis <scdavis41@gmail.com>
2024-09-04 06:49:01 -0400

    Display avatar in header

---
+++ src/pages/api/profile/avatar.ts
@@ -0,0 +1,27 @@
+import { getCurrentUserProfile } from "@/utils/auth";
+import { getStore } from "@netlify/blobs";
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async ({ cookies }) => {
+  const currentUserProfile = await getCurrentUserProfile();
+  if (!currentUserProfile) {
+    return new Response("User is not logged in", { status: 404 });
+  }
+
+  const userAvatarStore = getStore({
+    name: "UserAvatar",
+    consistency: "strong",
+  });
+  const userAvatarBlob = await userAvatarStore.get(
+    currentUserProfile.id.toString(),
+    {
+      type: "stream",
+    },
+  );
+
+  if (!userAvatarBlob) {
+    return new Response("Avatar not found", { status: 404 });
+  }
+
+  return new Response(userAvatarBlob);
+};
