9ea04e72cefbd99a3dcaade55a7bee880ab6a728
Sean C Davis <scdavis41@gmail.com>
2024-09-10 21:28:09 -0400

    Setup storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,35 @@
 import netlify from '@astrojs/netlify'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import storyblok from '@storyblok/astro'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import { defineConfig } from 'astro/config'
+import dotEnv from 'dotenv'
+
+dotEnv.config()
 
 export default defineConfig({
   output: 'hybrid',
   adapter: netlify(),
-  integrations: [tailwind(), react()],
+  integrations: [
+    tailwind(),
+    react(),
+    storyblok({
+      accessToken: process.env.STORYBLOK_TOKEN,
+      apiOptions: { region: 'us' },
+      components: {
+        page: 'storyblok/Page',
+        feature: 'storyblok/Feature',
+        grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
+      },
+    }),
+  ],
   experimental: {
     serverIslands: true,
   },
   vite:
-    process.env.DEV_SSL === 'true'
+    process.env.USE_SSL === 'true'
       ? {
           plugins: [basicSsl()],
           server: {
